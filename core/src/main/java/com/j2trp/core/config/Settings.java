dadc5fcfb8ab9b28311d96a7db4bcbb49a81930e
Daniel Roig <droig75@gmail.com>
2013-05-20 10:15:40 +0200

    Some minor tweaks and improvements.

--- core/src/main/java/com/j2trp/core/config/Settings.java
+++ core/src/main/java/com/j2trp/core/config/Settings.java
@@ -21,26 +21,25 @@ public class Settings {
   private static final Logger LOG = Logger.getLogger(Settings.class);
   private static final Object SINGLETON_LOCK = new Object();
   private static Thread backgroundThread;
+  // TODO: Investigate whether or not props could be declared volatile instead. 
   AtomicReference<Properties> props = new AtomicReference<Properties>();
   
   public Settings (File configFile) throws IllegalArgumentException, IOException {
     
     props.set(loadFile(configFile));
     
-    LOG.info("Trying to get the default file system object...");
     FileSystem fs = FileSystems.getDefault();
-    LOG.info("File system is : " + fs.provider().getScheme());
     
     try {
-      LOG.info("Trying to create new watch service...");
       WatchService watchService = fs.newWatchService();
-      LOG.info("Trying to create new watch service...done.");
+
       Path pathToConfigFile = configFile.toPath();
       Path pathToConfifFileDirectory = pathToConfigFile.getParent();
       pathToConfifFileDirectory.register(watchService, 
           StandardWatchEventKinds.ENTRY_MODIFY, 
           StandardWatchEventKinds.ENTRY_DELETE, 
           StandardWatchEventKinds.ENTRY_CREATE);
+      
       synchronized (SINGLETON_LOCK) {
         
         if (backgroundThread != null) {
@@ -50,6 +49,7 @@ public class Settings {
         backgroundThread = new Thread(new PropertiesFileWatcher(watchService, configFile, props), "PropertiesFileWatcher thread");
         backgroundThread.setDaemon(true);
         backgroundThread.start();
+        LOG.info("Started background thread");
       }
     }
     catch (UnsupportedOperationException e) {
@@ -70,39 +70,46 @@ public class Settings {
     
     @Override
     public void run() {
-      while (true) {
+      
+      boolean active = true;
+      
+      while (active) {
         try {
           WatchKey key = watcher.take();
           for (WatchEvent<?> event : key.pollEvents()) {
-            WatchEvent<Path> ev = cast(event);
             
-            if (ev.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+            if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
               LOG.warn("Config file has been deleted, retaining the current settings in memory.");
             }
-            else if (ev.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+            else if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
               LOG.info("Config file has been modified, initiating reload...");
               try {
                 propObj.set(loadFile(propertiesFile));
+                LOG.info(String.format("Property file %s successfully reloaded, %d entries read.", propertiesFile, propObj.get().size()));
               }
               catch (IOException e) {
                 LOG.error("I/O Exception when trying to reload file, retaining the current settings in memory.", e);
               }
             }
-            else if (ev.kind() == StandardWatchEventKinds.ENTRY_CREATE ) { 
+            else if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE ) { 
               LOG.info("Config file has been recreated, initiating reload...");
               try {
                 propObj.set(loadFile(propertiesFile));
+                LOG.info(String.format("Property file %s successfully reloaded, %d entries read.", propertiesFile, propObj.get().size()));
               }
               catch (IOException e) {
                 LOG.error("I/O Exception when trying to reload file, retaining the current settings in memory.", e);
               }
             }
           }
+          key.reset();
           
         }
         catch (InterruptedException e) {
           LOG.warn(Thread.currentThread().getName() + " was interrupted. Changes in the settings file will no longer be detected.");
           // TODO: watcher.close();
+          active = false;
+          break;
         }
       }
     }
@@ -116,21 +123,6 @@ public class Settings {
     
     try (InputStream is = new FileInputStream(configFile)) {
       result.load(is);
-    }
-    
-    return result;
-  }
-  
-  @SuppressWarnings("unchecked")
-  static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-      return (WatchEvent<T>)event;
-  }
-  
-  static String[] getPathComponents (Path path) {
-    
-    String[] result = new String[path.getNameCount()];
-    for (int i = 0 ; i < result.length; i++) {
-      result[i] = path.getName(i).toString();
     }
     
     return result;
--- core/src/test/java/com/j2trp/core/config/SettingsTest.java
+++ core/src/test/java/com/j2trp/core/config/SettingsTest.java
@@ -1,6 +1,7 @@
 package com.j2trp.core.config;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Properties;
@@ -18,32 +19,44 @@ public class SettingsTest {
     Properties props = new Properties();
     props.setProperty("Key1", "Value1");
     
-    try (PrintWriter pw = new PrintWriter(tempFile)) {
-      props.store(pw, "");
-    }
+    updatePropFile(props, tempFile);
     
     Settings settings = new Settings(tempFile);
-    String[] strs = Settings.getPathComponents(tempFile.toPath().getParent());
-    for (String str : strs) {
-      System.out.println(str);
-    }
-    
+
     Assert.assertEquals(settings.getProperty("Key1"), "Value1");
     Assert.assertNull(settings.getProperty("Key2"));
     
     Thread.sleep(3000);
     
+    // Try updating the file.
     props.setProperty("Key2", "Value2");
     
-    try (PrintWriter pw = new PrintWriter(tempFile)) {
-      props.store(pw, "");
-    }
+    updatePropFile(props, tempFile);
     
     Thread.sleep(10000);
     Assert.assertNotNull(settings.getProperty("Key1"));
     Assert.assertEquals(settings.getProperty("Key2"), "Value2");
     
+    // Try deleting the file.
+    tempFile.delete();
+    Thread.sleep(10000);
+    Assert.assertNotNull(settings.getProperty("Key1"));
+    Assert.assertEquals(settings.getProperty("Key2"), "Value2");
     
+    
+    // Try recreating the file.
+    props.setProperty("Key2", "Value2.2");
+    updatePropFile(props, tempFile);
+    Thread.sleep(10000);
+    Assert.assertNotNull(settings.getProperty("Key1"));
+    Assert.assertEquals(settings.getProperty("Key2"), "Value2.2");
+  }
+
+  private static void updatePropFile(Properties props, File tempFile) throws IOException,
+      FileNotFoundException {
+    try (PrintWriter pw = new PrintWriter(tempFile)) {
+      props.store(pw, "");
+    }
   }
   
   
